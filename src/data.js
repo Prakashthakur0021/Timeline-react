b404563bd5d0ab48e126429f00212187c62295a6
Prakash Thakur <prakashthakur0021@gmail.com>
2020-07-01 00:32:40 -0700

    init

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    text: "This is the text from the data.js ",
+    date: "April 13 2000",
+    category: {
+      tag: "web-idea",
+      color: "FFDB14",
+    },
+    link: {
+      url: "http://github.com/",
+      text: "Check it out",
+    },
+  },
+  {
+    text: "This is the other text some random ",
+    date: "June 43 2000",
+    category: {
+      tag: "html",
+      color: "D4FF14",
+    },
+    link: {
+      url: "http://github.com/",
+      text: "Check it out",
+    },
+  },
+  {
+    text: "This is the third text from data.js ",
+    date: "July 12 2000",
+    category: {
+      tag: "CSS",
+      color: "14DBFF",
+    },
+    link: {
+      url: "http://github.com/",
+      text: "Check it out",
+    },
+  },
+  {
+    text: "This is the last text  ",
+    date: "May 34 2000",
+    category: {
+      tag: "web-design",
+      color: "FDBD41",
+    },
+    link: {
+      url: "http://github.com/",
+      text: "Check it out",
+    },
+  },
+];
+
+export default data;
